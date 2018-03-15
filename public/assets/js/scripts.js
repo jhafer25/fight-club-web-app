4354fc0862db6ce450e14535d485fe81e92e23b1
James Hafer <jhafer25@gmail.com>
2018-03-14 21:02:33 -0600

    Deployment updates

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   		auto: true,
   	});
   	console.log($('footer').baseURL);
-  	if(location.href == "http://localhost:3000/signin" || location.href == "http://localhost:3000/home" || location.href == "http://localhost:3000/fightMatcher"){
+  	if(location.href == "https://fight-club-web-app.herokuapp.com/signin" || location.href == "https://fight-club-web-app.herokuapp.com/home" || location.href == "https://fight-club-web-app.herokuapp.com/fightMatcher"){
   		$('footer').css("position", "absolute");
   		$('footer').css("bottom", "0");
 		$('footer').css("width", "100%");
